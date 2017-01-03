de0cb8baaa3404e89146526829fda22bc15e05d7
sxend <arimitsu.k+github@gmail.com>
2017-01-03 13:55:37 +0900

    add initial files

---
+++ lib/index.ts
@@ -0,0 +1,2 @@
+
+window["sofr"] = window["sofr"] || {};
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+const path = require('path');
+const env = process.env['npm_config_env'] || "production";
+const preprocessOptJson = JSON.stringify(({
+  "local": {},
+  "development": {},
+  "staging": {},
+  "production": {}
+})[env]);
+module.exports = {
+  context: path.join(__dirname, 'lib'),
+  entry: './index.ts',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'sofr.js'
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: ['', '.webpack.js', '.html', '.js', '.tsx']
+  },
+  module: {
+    loaders: [{
+      test: /\.tsx$/,
+      exclude: /(node_modules)/,
+      loader: 'ts-loader!preprocess?' + preprocessOptJson
+    }, {
+      test: /\.html$/,
+      loader: "html-loader"
+    }],
+    preLoaders: [{
+      test: /\.js$/,
+      loader: "source-map-loader"
+    }]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
+};
